7e1ee9e41410023dfe867b92e1e87656c4da29db
Ben <bensimmo@gmail.com>
2019-04-21 11:17:13 +0100

    major update
    a lot of updates, not everything is sone.   SDS0x1 sleeping is disabled as it trips most RaspberryPi board.

--- NODES/RPi/monitAirPi/monitAir Python3/monitairpi.py
+++ NODES/RPi/monitAirPi/monitAir Python3/monitairpi.py
@@ -1,61 +1,105 @@
 #!/usr/bin/python3
+'''
+Full Payload Structure
+    payload = {"timestamp":-1,
+               "dev": CHAtP-dddddddd-x,
+               "temp": ##.#,
+               "humidity": ##,
+               "pressure": ####,
+               "PM10" : ###,
+               "PM25" : ###,
+               "iaq": ###,
+               "dsTemp": ##.#,
+               "long": ##.#####,
+               "lat": ##.#####,
+               "alt": ###
+               }
+'''
 
-from time import sleep, gmtime, strftime, time
+from time import sleep, time
+from datetime  import datetime, timezone
 import subprocess
-import mqttclient	#sudo pip3 install paho-mqtt
-from sds011 import SDS011	#local module sds011.py
+import mqttclient  # local module, also need : sudo pip3 install paho-mqtt
 import json
 from statistics import median
-from w1thermsensor import W1ThermSensor		#sudo pip3 install w1thermsensor
-import serial	#sudo pip3 install pyserial 
-import pynmea2	#sudo pip3 install pynmea2
+import serial  # sudo pip3 install pyserial #GPIO/USB Serial read
 
 
+######                                               ######
+# ConnectedHumber : MonitAIR project authentication info. #
+######                                               ######
 
-
-# ConnectedHumber : MonitAIR project authentication info.
-
-MQTT_USERNAME  = "ch-username"
-MQTT_CLIENT_ID = "name_pi01"
+MQTT_USERNAME  = "connectedhumber"
+MQTT_PASSWORD  = "3fds8gssf6"
+MQTT_CLIENT_ID = "" #CHAtp-dddddddd-x blank for autogenerate
 MQTT_HOSTNAME = "mqtt.connectedhumber.org"
 MQTT_PORT = 1883
 MQTT_TOPIC = "airquality/data"
 
-GNSS_PORT = "/dev/ttyACM0"
+# Below enables sensors, set to "" to diasble or provide port/file name 
+GNSS_PORT = ""  # "/dev/ttyACM0"
 SDS_PORT = "/dev/serial0"
 BME_BSEC = "./bsec_bme680"
 
-payload = {"timestamp":-1,
-           "dev":MQTT_CLIENT_ID,
-           "temp":-1,
-           "humidity":-1,
-           "pressure":-1,
-           "PM10" : -1,
-           "PM25" : -1,
-           "iaq":-1,
-           "groundTemp":-1,
-           "lon":-1,
-           "lat":-1,
-           "alt":-1
-           }
+W1THERM = True
+
+DEVICE_TYPE = "S"  # "M" for mobile
+LOCATION_NUMBER = "1" # increase if location is moved for the same device
+
+if GNSS_PORT is not "":
+    import pynmea2  # suso pip3 pynmea2     NMEA GNSS splitter
+
+if SDS_PORT is not "":
+    from sds011 import SDS011  # local module for SDS011/021 PM sensors
+
+if W1THERM is True:
+    from w1thermsensor import W1ThermSensor  #sudo pip3 install w1thermsensor     DS18B20 
+
+
+payload = {}
 
 #general
-timestamp = 0.0
-loop_delay = 2 #seconds
-reading_delay = 60*6 # time between sending data, every 6 minutes for monitAIR MQTT
-timestart = 0 
+loop_delay = 0.05 #seconds
+reading_delay = 60*6 # time between sending data
+timestart = 0
+waittime = 55  #initial estimate for time to perform all measurments
+
+def get_serial():
+    # Extract serial from cpuinfo file
+    cpuserial = "N0N0N0N0"
+    try:
+        f = open('/proc/cpuinfo','r')
+        for line in f:
+            if line[0:6]=='Serial':
+                cpuserial = line[18:26]
+        f.close()
+    except:
+        cpuserial = "ERROR000"
+
+    return cpuserial
+
+
+def get_clientid():
+    clientid = list("CHAtR-dddddddd-x")
+    clientid[3] = DEVICE_TYPE
+    clientid[6:14] = get_serial()
+    clientid[15] = LOCATION_NUMBER
+
+    return "".join(clientid)
+
 
 def sds_start():
-    # details about it
-    print(sds_sensor.device_id)
-    print(sds_sensor.firmware)
-    print(sds_sensor.dutycycle)
+#SDS0x1
+    print("about to create")
+    # Now we have some details about it
+    print("SDS011 sensor info:")
+    print("Device ID: ", sds_sensor.device_id)
+    print("Device firmware: ", sds_sensor.firmware)
+    print("Current device cycle (0 is permanent on): ", sds_sensor.dutycycle)
     print(sds_sensor.workstate)
     print(sds_sensor.reportmode)
-    # Set dutycyle to nocycle (permanent)
     sds_sensor.reset()
     sds_sensor.workstate = SDS011.WorkStates.Measuring
-    #warm up delay removed as built in to main loop, add sleep(15) if using a fast loop
 
 def bme680_go():
     for line in iter(proc.stdout.readline, ''):
@@ -64,125 +108,175 @@ def bme680_go():
 
         listIAQ_Accuracy.append(int(lineDict['IAQ_Accuracy']))
         listPressure.append(float(lineDict['Pressure']))
-        listGas.append(int(lineDict['Gas']))
         listTemperature.append(float(lineDict['Temperature']))
         listIAQ.append(float(lineDict['IAQ']))
         listHumidity.append(float(lineDict['Humidity']))
-        listStatus.append(int(lineDict['Status']))
 
-        if len(listIAQ_Accuracy) == 20:
-            #generate the median for each value
-            IAQ_Accuracy = median(listIAQ_Accuracy)
+        #client.loop()
+        if len(listIAQ_Accuracy) == 10:
+            # generate the median for each value
+
             Pressure = median(listPressure)
-            Gas = median(listGas)
             Temperature = median(listTemperature)
             IAQ = median(listIAQ)
             Humidity = median(listHumidity)
-            Status = median(listStatus)
-            #clear lists
+
+            # clear lists
             listIAQ_Accuracy.clear()
             listPressure.clear()
-            listGas.clear()
             listTemperature.clear()
             listIAQ.clear()
             listHumidity.clear()
-            listStatus.clear()
-            #Temperature Offset, quick and dirt method.  Best set in the BSEC for more accurate results.
-            Temperature = Temperature + 0
+
+            # Temperature Offset
+            # Temperature = Temperature + 0
             return round(Temperature, 1), round(Humidity,1), int(Pressure), int(IAQ)
+
+if GNSS_PORT is not "":
+    def gnss_go():
+        while True:
+            strip = serialPort.readline()
+            if strip.find(b'GGA') > 0:
+                msg = pynmea2.parse(strip.decode('utf-8'))
+                break
+            sleep(0.01)
+        return round(msg.longitude,5), round(msg.latitude,5), msg.altitude
+
+def sds_go():
+
+    for count in range(30):
+        client.loop()
+        try:
+            PM_data = sds_sensor.get_values()
+        except (IOError, OSError) as e:
+            print("Error happened. {}".format(e.args[-1]))
+            PM_data = None
+        if PM_data is not None:
+            list_PM25.append(float(PM_data[1]))
+            list_PM10.append(float(PM_data[0]))
+            sleep(0.01)
+        else:
+            print("None Error")
         
-def gnss_go():
-    print("gnss is go")
-    #serialPort = serial.Serial(GNSS_PORT, 9600, timeout=0.5)
-    while True:
-        strip = serialPort.readline()
-        #print(strip)
-        if strip.find(b'GGA') > 0:
-            msg = pynmea2.parse(strip.decode('utf-8'))
-            #print(msg.latitude,msg.longitude,msg.altitude)
-            break
-        sleep(0.01)
-    return round(msg.longitude,5),round(msg.latitude,5),msg.altitude, msg.timestamp
+    # generate the median for each value, return -100 error on no data
+    if len(list_PM25) == 0 or len(list_PM10) == 0:
+        PM25 = -100
+        PM10 = -100
+    else:
+        PM25 = int(median(list_PM25))
+        PM10 = int(median(list_PM10))
+
+    print("PM25 list ", list_PM25 , "= ",PM25)
+    print("PM10 list ", list_PM10 , "= ",PM10)
+    # clear lists
+    list_PM25.clear()
+    list_PM10.clear()
+    # sds_sensor.workstate = SDS011.WorkStates.Sleeping
+    return PM25, PM10
+
 
 def timestamp_format():
-    return strftime("%a %b %d %Y %H:%M:%S GMT+0000",gmtime())
+    # 2018-08-25T14:23:45+00:00
+    return datetime.now(timezone.utc).replace(microsecond=0).isoformat()
 
 def on_message(message):
     print("on_message received: " + str(message))
-    
+
 def on_connect(message):
-    print("on_connect received: " + str(message))    
+    print("on_connect received: " + str(message))
 
 
 #-----Main
-#Connect to MQTT 
+#Connect to MQTT
+
+if MQTT_CLIENT_ID == "":
+    payload["dev"] = get_clientid()
+else:
+    payload["dev"] = MQTT_CLIENT_ID
+
+
 client = mqttclient.monitairMQTTClient()
 client.on_message = on_message
 client.on_connect = on_connect
 client.begin(MQTT_USERNAME, MQTT_PASSWORD, MQTT_CLIENT_ID, MQTT_HOSTNAME, MQTT_PORT)
 
-print("sleep 2 seconds for connect just incase..")
-sleep(2)
-print("slept")
-
-
-
-
 print("--- monitAIR Monitoring ---")
 
-#BME680
-print("BME680 Humidity, Pressure, Air Temperature, IAQ Start")
-proc = subprocess.Popen([BME_BSEC], stdout=subprocess.PIPE)
-listIAQ_Accuracy = []
-listPressure = []
-listGas = []
-listTemperature = []
-listIAQ = []
-listHumidity  = []
-listStatus = []
+if BME_BSEC is not "":
+    #BME680
+    print("BME680 Humidity, Pressure, Air Temperature, IAQ Start")
+    proc = subprocess.Popen([BME_BSEC], stdout=subprocess.PIPE)
+    listIAQ_Accuracy = []
+    listPressure = []
+    listTemperature = []
+    listIAQ = []
+    listHumidity  = []
 
-#DS18B20
-print("DS18B20 Ground Temperature start")
-w1sensor = W1ThermSensor()
+if W1THERM is True:
+    #DS18B20
+    print("DS18B20 Ground Temperature start")
+    w1sensor = W1ThermSensor()
 
 #GNSS
-print("GNSS start")
-serialPort = serial.Serial(GNSS_PORT, 9600, timeout=0.5)
+if GNSS_PORT is not "":
+    print("GNSS start")
+    serialPort = serial.Serial(GNSS_PORT, 9600, timeout=1.5)
 
-#SDS0x1
-print("SDS0x1 PM2.5 & PM10 Start")
-sds_sensor = SDS011(SDS_PORT)
-print("port opened")
-sds_start()
+if SDS_PORT is not "":
+    print("SDS0x1 Start")
+    sds_sensor = SDS011(SDS_PORT, timeout=2)
+    print("port opened")
+    sds_start()
+    list_PM25 = []
+    list_PM10 = []
 
 try:
-
     print(payload)
     while True:
         client.loop()
-        if (time() > timestart + reading_delay):
-            bme_data = bme680_go()
-            sds_data = sds_sensor.get_values()
-            gnss_data = gnss_go()
-            gtemp_data = round(w1sensor.get_temperature(),1)
-            
-            payload["temp"] = bme_data[0]
-            payload["humidity"] = bme_data[1]
-            payload["pressure"] = bme_data[2]
-            payload["iaq"] = bme_data[3]
-           
-            payload["PM25"] = int(sds_data[1])
-            payload["PM10"] = int(sds_data[0])
-            payload["groundTemp"]= gtemp_data
-            payload["lon"] = gnss_data[0]
-            payload["lat"] = gnss_data[1]
-            payload["alt"] = gnss_data[2]
-            payload["timestamp"] = timestamp_format() #gnss_data[3] in future
+        if (time() > timestart + reading_delay - waittime):
+            timecheck = time()
+            # print("timecheck = ", timecheck)
+
+            if SDS_PORT is not "":
+                #sds_sensor.workstate = SDS011.WorkStates.Measuring
+                warmuptime = time()
+
+            if BME_BSEC is not "":
+                bme_data = bme680_go()
+                payload["temp"] = bme_data[0]
+                payload["humidity"] = bme_data[1]
+                payload["pressure"] = bme_data[2]
+                payload["iaq"] = bme_data[3]
+
+            if GNSS_PORT is not "":
+                gnss_data=gnss_go()
+                payload["lon"] = gnss_data[0]
+                payload["lat"] = gnss_data[1]
+                payload["alt"] = gnss_data[2]
+
+            if W1THERM is True:
+                gtemp_data = round(w1sensor.get_temperature(),1)
+                payload["dsTemp"]= gtemp_data
+
+            if SDS_PORT is not "":
+                while (time() < warmuptime + 29):
+                    sleep(0.01)
+                sds_data = sds_go()
+                payload["PM25"] = int(sds_data[0])
+                payload["PM10"] = int(sds_data[1])
+
+            payload["timestamp"] = timestamp_format()
+
+            waittime = time() - timecheck - 1
+            # print("waittime = ", waittime)
 
             client.jsonWrite(MQTT_TOPIC, payload)
+            # print("MQTT on it's way ::: ", payload)
+
             timestart = time()
         sleep(loop_delay)
 
 except KeyboardInterrupt:
-    sds_sensor.workstate = SDS011.WorkStates.Sleeping
-    pass
+    sds_sensor.reset()
+    sds_sensor = None
